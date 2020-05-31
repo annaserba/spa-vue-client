4e6dd4c0de38e01558d424f4474832fa6ed29933
annaserba <60755945+annaserba@users.noreply.github.com>
2020-05-31 16:18:39 +0300

    add users

---
+++ components/User/users.vue
@@ -0,0 +1,34 @@
+<template>
+  <Table border :columns="columns" :data="users" :loading="loading"></Table>
+</template>
+<script>
+export default {
+  props: {
+    loading: Boolean,
+    users: {
+      type: Array,
+      default: null
+    },
+    columns: {
+      type: Array,
+      default() {
+        return [
+          {
+            title: '#',
+            key: 'id',
+            sortable: true
+          },
+          {
+            title: 'Username',
+            key: 'userName'
+          },
+          {
+            title: 'Department',
+            key: 'department'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,47 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        spa-vue-client
-      </h1>
-      <h2 class="subtitle">
-        Welcome to the iView + Nuxt.js template
-      </h2>
-      <div class="links">
-        <Button type="primary" target="_blank" to="https://nuxtjs.org/">
-          Documentation
-        </Button>
-        <Button target="_blank" to="https://github.com/nuxt/nuxt.js">
-          GitHub
-        </Button>
-        <Button target="_blank" to="https://www.iviewui.com/">
-          iView
-        </Button>
-      </div>
-    </div>
-  </div>
+  <Row type="flex" justify="center">
+    <Col :xs="{ span: 24 }" :md="{ span: 12 }">
+      <Button to="/add" :departments="departments">Add </Button>
+      <Users :loading="loading" :users="users" />
+    </Col>
+  </Row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import axios from 'axios'
+import Users from '~/components/User/users.vue'
+
 export default {
   components: {
-    Logo
+    Users
+  },
+  asyncData({ app, env }) {
+    return {
+      USER_URL: env.USER_URL,
+      DEPARTMENT_URL: env.DEPARTMENT_URL
+    }
+  },
+  data() {
+    return {
+      users: [],
+      departments: [],
+      loading: true
+    }
+  },
+  mounted() {
+    Promise.all([
+      axios.get(this.USER_URL + '/api/users'),
+      axios.get(this.DEPARTMENT_URL + '/api/departments')
+    ]).then(([users, departments]) => {
+      this.users = users.data.map(({ id, userName, departmentId }) => {
+        const departmentName = departments.data.filter(
+          (department) => department.id === departmentId
+        )
+        return { id, userName, departmentName }
+      })
+      this.departments = departments.data
+      this.loading = false
+    })
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links {
-  padding-top: 15px;
-}
-</style>
