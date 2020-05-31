fdaf6837faacc3ea82dcc5c94d0e3413c4dcd639
annaserba <60755945+annaserba@users.noreply.github.com>
2020-05-31 21:50:57 +0300

    add store

---
+++ store/user.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  state: { select: null },
+  getters: {
+    getSelectId(state) {
+      return state.select ? state.select.id : 0
+    }
+  },
+  mutations: {
+    setSelect(state, select) {
+      state.select = select
+    }
+  },
+  actions: {
+    setSelect(state, select) {
+      state.commit('setSelect', select)
+    }
+  }
+}
+</script>
